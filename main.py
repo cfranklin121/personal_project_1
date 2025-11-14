6e3ffbf28ad4465ab350bcd47b70aa30d543755f
cfranklin121 <courtenayfranklin@gmail.com>
2025-11-13 20:17:51 -0700

    Initial concepts

---
+++ main.py
@@ -0,0 +1,10 @@
+pantry = [{"eggs": 12}, {"milk": 1}, {"flower": 5}]
+
+recipies = [
+    [{"cake": "*"}, {"eggs": 1}, {"milk": 1}, {"flower": 5}],
+    [{"spagetti": "*"}, {"pasta": 1}, {"tomatoes": 2}]
+]
+
+print(pantry)
+for recipie in recipies:
+    print(recipie)
