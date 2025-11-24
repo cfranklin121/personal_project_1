3f7497c0f39c95bfc904265e3affe89ec47617e1
cfranklin121 <courtenayfranklin@gmail.com>
2025-11-23 20:23:56 -0700

    Minor tweaks to help.py

--- help.py
+++ help.py
@@ -4,5 +4,4 @@ def help():
     os.system('clear')
     print("Help dialog will go here.")
     print("Enter 1 through 6 for menu selections.")
-    input("Press any key to continue...")
     return
